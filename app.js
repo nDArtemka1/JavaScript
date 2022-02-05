158c8f82bd7bc0b00cafd59ddfaec595777eb702
nDArtemka1 <82649441+nDArtemka1@users.noreply.github.com>
2022-02-05 20:57:20 +0300

    заменил

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+'use strict';
+
 let fitlerPopup = document.querySelector('.filterPopup');
 let fitlerLabel = document.querySelector('.filterLabel');
 let filterIcon = document.querySelector('.filterIcon');
 
-fitlerLabel.addEventListener('click', function () {
+fitlerLabel.addEventListener('click', function() {
     fitlerPopup.classList.toggle('hidden');
     fitlerLabel.classList.toggle('filterLabelPink');
     filterIcon.classList.toggle('filterIconPink');
@@ -15,14 +17,14 @@ fitlerLabel.addEventListener('click', function () {
 });
 
 let filterHeaders = document.querySelectorAll('.filterCategoryHeader');
-filterHeaders.forEach(function (header) {
-    header.addEventListener('click', function (event) {
+filterHeaders.forEach(function(header) {
+    header.addEventListener('click', function(event) {
         event.target.nextElementSibling.classList.toggle('hidden');
     })
 });
 
 let filterSizes = document.querySelector('.filterSizes');
 let filterSizeWrap = document.querySelector('.filterSizeWrap');
-filterSizeWrap.addEventListener('click', function () {
+filterSizeWrap.addEventListener('click', function() {
     filterSizes.classList.toggle('hidden');
 });
