5940a6cfd661498f79c042cc8dc7b4d334afe66d
nDArtemka1 <82649441+nDArtemka1@users.noreply.github.com>
2022-02-06 22:17:01 +0300

    доделал 8 дз

---
+++ scripts/basket/products.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const products = [
+    new ProductDTO(
+        0,
+        '0.jpg',
+        'Product 0',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        52.22
+    ),
+    new ProductDTO(
+        1,
+        '1.jpg',
+        'Product 1',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        32.42
+    ),
+    new ProductDTO(
+        2,
+        '2.jpg',
+        'Product 2',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        42.42
+    ),
+    new ProductDTO(
+        3,
+        '3.jpg',
+        'Product 3',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        72.42
+    ),
+    new ProductDTO(
+        4,
+        '4.jpg',
+        'Product 4',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        92.42
+    ),
+    new ProductDTO(
+        5,
+        '5.jpg',
+        'Product 5',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        12.42
+    ),
+]
